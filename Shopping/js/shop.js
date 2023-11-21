6d3e44e4f1b294d09e12b47ece703997b2f144c2
swk802@gmail.com <swk802@gmail.com>
2023-11-21 22:47:57 +0530

    Shopping App Completed

--- Shopping/js/shop.js
+++ Shopping/js/shop.js
@@ -2,7 +2,7 @@ var obj = [
     { id: 1, Pname: "laptop", category: "laptop", subcategory: "hp", price: 30000, Pimage: "../images/l1.jpeg" },
     { id: 2, Pname: "laptop", category: "laptop", subcategory: "hp", price: 32000, Pimage: "../images/l2.png" },
     { id: 3, Pname: "laptop", category: "laptop", subcategory: "dell", price: 20000, Pimage: "../images/l3.jpeg" },
-    { id: 4, Pname: "laptop", category: "laptop", subcategory: "dell", price: 25000, Pimage: "../images/l4.jpeg" },
+    { id: 4, Pname: "laptop", category: "laptop", subcategory: "dell", price: 25000, Pimage: "../images/l4.png" },
     { id: 5, Pname: "laptop", category: "laptop", subcategory: "hp", price: 35000, Pimage: "../images/l5.jpeg" },
 
     { id: 6, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m1.jpeg" },
@@ -10,9 +10,56 @@ var obj = [
     { id: 8, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 25000, Pimage: "../images/m3.jpeg" },
     { id: 9, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 30000, Pimage: "../images/m4.jpeg" },
     { id: 10, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 10000, Pimage: "../images/m5.jpeg" },
-    { id: 11, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w1.jpeg" },
 
-    { id: 12, Pname: "watch", category: "watch", price: 3000, Pimage: "../images/w2.jpeg" }
+    { id: 11, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w1.jpeg" },
+    { id: 12, Pname: "watch", category: "watch", price: 3000, Pimage: "../images/w2.jpeg" },
+
+    { id: 13, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s1.jpeg" },
+    { id: 14, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s2.jpeg" },
+    { id: 15, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s3.jpeg" },
+    { id: 16, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s4.jpeg" },
+    { id: 17, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s5.jpeg" },
+    { id: 18, Pname: "Cloth", category: "Cloth", subcategory: "Shirt", price: 30000, Pimage: "../images/s6.jpeg" },
+
+
+    { id: 19, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h1.jpeg" },
+    { id: 20, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h2.jpeg" },
+    { id: 21, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h3.jpeg" },
+    { id: 22, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h4.jpeg" },
+    { id: 23, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h5.jpeg" },
+    { id: 24, Pname: "Headphone", category: "Electronics", subcategory: "Headphone", price: 32000, Pimage: "../images/h6.jpeg" },
+
+    { id: 25, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl1.jpeg" },
+    { id: 26, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl2.jpeg" },
+    { id: 27, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl3.jpeg" },
+    { id: 28, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl4.jpeg" },
+    { id: 29, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl5.jpeg" },
+    { id: 30, Pname: "Bottle", category: "Pot", subcategory: "Bottle", price: 32000, Pimage: "../images/btl6.jpeg" },
+
+    { id: 31, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b1.jpeg" },
+    { id: 32, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b2.jpeg" },
+    { id: 33, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b3.jpeg" },
+    { id: 34, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b4.jpeg" },
+    { id: 35, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b5.jpeg" },
+    { id: 36, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b6.jpeg" },
+    { id: 37, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b7.jpeg" },
+    { id: 38, Pname: "Book", category: "Book", subcategory: "Book", price: 32000, Pimage: "../images/b8.jpeg" },
+
+
+    { id: 39, Pname: "laptop", category: "laptop", subcategory: "dell", price: 20000, Pimage: "../images/l7.jpeg" },
+    { id: 40, Pname: "laptop", category: "laptop", subcategory: "dell", price: 25000, Pimage: "../images/l8.jpeg" },
+    { id: 41, Pname: "laptop", category: "laptop", subcategory: "hp", price: 35000, Pimage: "../images/l9.jpeg" },
+
+    { id: 42, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m6.jpeg" },
+    { id: 43, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m7.jpeg" },
+    { id: 44, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m8.jpeg" },
+    { id: 45, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m9.jpeg" },
+
+    { id: 46, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w5.jpeg" },
+    { id: 47, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w6.jpeg" },
+    { id: 48, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w7.jpeg" },
+    { id: 49, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w8.jpeg" },
+    { id: 50, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w9.jpeg" },
 
 ]
 
@@ -22,8 +69,8 @@ function filterData(cat) {
     })
     var totaldata = fdata.map((item) => {
         return (`
-            <div class="left col-3">
-                <img src=${item.Pimage} height="150px">
+            <div class="left col-3 box">
+                <img src=${item.Pimage} height="150px" width="150px">
                 <div class="content">
                     <h2> Product-${item.Pname}   </h2>
                     <p>Price-Rs.${item.price} category=${item.category}  </p>
@@ -43,7 +90,7 @@ function laptopfilter(subcat) {
     var totaldata = sbdata.map((item) => {
         return (`
             <div class="left col-3">
-                <img src=${item.Pimage} height="150px">
+                <img src=${item.Pimage} height="150px" width="150px">
                 <div class="content">
                     <h2> Product-${item.Pname}   </h2>
                     <p>Price-Rs.${item.price} category=${item.category}  </p>
@@ -59,7 +106,7 @@ function laptopfilter(subcat) {
 var totaldata = obj.map((item) => {
     return (`
         <div class="left col-3">
-            <img src=${item.Pimage} height="150px">
+            <img src=${item.Pimage} height="150px" width="150px">
             <div class="content">
                 <h2> Product-${item.Pname}   </h2>
                 <p>Price-Rs.${item.price} category=${item.category}  </p>
@@ -71,3 +118,21 @@ var totaldata = obj.map((item) => {
     `)
 })
 document.getElementById("root").innerHTML = totaldata;
+
+var btn = document.getElementById("topBtn")
+// window.onscroll = function data() {
+//     if (document.body.scrollTop > 200) {
+//         btn.style.display = "block"
+//     } else {
+//         btn.style.display = "none"
+//     }
+// }
+
+function gototop() {
+    window.scrollTo(
+        {
+            top: 0,
+            behaviour: "smooth"
+        }
+    )
+}
